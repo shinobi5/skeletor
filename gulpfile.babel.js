ff1c24ceee5ec79d569fd7cf230d1b4ff5ac9e38
David White <david_white@bnz.co.nz>
2019-01-11 01:07:14 +1300

    add babel

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -15,7 +15,7 @@ var gulp = require('gulp'),
     nunjucksRender = require('gulp-nunjucks-render');
 
 // clean
-gulp.task('clean:all', function () {
+gulp.task('clean:all', () => {
     return del([
         'node_modules',
         'dist',
@@ -25,30 +25,30 @@ gulp.task('clean:all', function () {
 });
 
 // clean: remove dist folder
-gulp.task('clean:dist', function () {
+gulp.task('clean:dist', () => {
     return del([
         'dist'
     ]);
 });
 
 // browser sync: setup server
-gulp.task('browserSync', function() {
+gulp.task('browserSync', () => {
     browserSync.init({
         server: {
           baseDir: 'dist'
         },
-    })
+    });
 });
 
 // lint: check js for errors
-gulp.task('lint', function() {
+gulp.task('lint', () => {
     return gulp.src('js/**/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 // nunjucks
-gulp.task('nunjucks', function() {
+gulp.task('nunjucks', () => {
 
   // gets the .nunjucks files in the pages directory
   return gulp.src('src/pages/**/*.nunjucks')
@@ -63,67 +63,67 @@ gulp.task('nunjucks', function() {
 });
 
 // useref:build - concatenate, minify and place JS refs into HTML
-gulp.task('useref:build', ['nunjucks'], function(){
+gulp.task('useref:build', gulp.series('nunjucks', () => {
     return gulp.src('src/pages/*.html')
         .pipe(useref())
 
         // minifies only if it's a JavaScript file
         .pipe(gulpIf('*.js', uglify()))
         .pipe(gulp.dest('dist'));
-});
+}));
 
 // useref:watch - concatenate, minify and place JS refs into HTML
-gulp.task('useref:watch', ['nunjucks'], function(){
+gulp.task('useref:watch', gulp.series('nunjucks', () => {
     return gulp.src('src/pages/*.html')
         .pipe(useref())
 
         // minifies only if it's a JavaScript file
         .pipe(gulpIf('*.js', uglify()))
         .pipe(gulp.dest('dist'));
-});
+}));
 
 // concat css files
-gulp.task('css', function() {
+gulp.task('css', () => {
     return gulp.src([
-        'src/css/settings/**/*.css',
-        'src/css/global/**/*.css',
-        'src/css/elements/**/*.css',
-        'src/css/objects/**/*.css',
-        'src/css/components/**/*.css',
-        'src/css/utilities/**/*.css'
+    'src/css/settings/**/*.css',
+    'src/css/global/**/*.css',
+    'src/css/elements/**/*.css',
+    'src/css/objects/**/*.css',
+    'src/css/components/**/*.css',
+    'src/css/utilities/**/*.css'
     ])
-        .pipe(concat('style.css'))
-        .pipe(cleanCSS())
-        .pipe(gulp.dest('dist'))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
+    .pipe(concat('style.css'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.reload({
+        stream: true
+    }));
 });
 
 // img: optimise images
-gulp.task('img', function(){
+gulp.task('img', () => {
     return gulp.src('src/img/**/*.+(png|jpg|jpeg|gif|svg)')
 
         // caching images that ran through imagemin
         .pipe(cache(imagemin({
-          interlaced: true
+            interlaced: true
         })))
         .pipe(gulp.dest('dist/img'))
 });
 
 // fonts: move font files from src to dist
-gulp.task('fonts', function() {
+gulp.task('fonts', () => {
     return gulp.src('src/font/**/*')
         .pipe(gulp.dest('dist/font'))
 });
 
 // watch: watch files For changes
-gulp.task('watch', ['browserSync', 'css'], function() {
-    gulp.watch('src/css/**/*.css', ['css'], browserSync.reload);
-    gulp.watch('src/templates/**/*.nunjucks', ['useref:watch'], browserSync.reload);
-    gulp.watch('src/pages/**/*.nunjucks', ['useref:watch'], browserSync.reload);
-    gulp.watch('src/js/**/*.js', ['lint', 'useref:watch'], browserSync.reload);
-});
+// gulp.task('watch', gulp.series(['css', 'browserSync'], function() {
+//     // gulp.watch('src/css/**/*.css', gulp.parallel('css', browserSyncReload));
+//     gulp.watch('src/templates/**/*.nunjucks', gulp.series('useref:watch', browserSync.reload));
+//     gulp.watch('src/pages/**/*.nunjucks', gulp.series('useref:watch', browserSync.reload));
+//     gulp.watch('src/js/**/*.js', gulp.series('lint', 'useref:watch', browserSync.reload));
+// }));
 
 // gulp: default task
-gulp.task('default', ['browserSync', 'useref:build', 'img', 'fonts', 'lint', 'css', 'watch']);
+gulp.task('default', gulp.series('useref:build', 'img', 'fonts', 'lint', 'css', 'browserSync', 'watch'));
