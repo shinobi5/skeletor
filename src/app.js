03423e77d6c3f7f623c685a23e93d6a19c22cb4f
David White <david_white@bnz.co.nz>
2019-03-22 22:47:24 +1300

    remove gulp and add parcel

--- src/app.js
+++
@@ -1,3 +0,0 @@
-import ExampleComponent from './components/ExampleComponent/ExampleComponent.js';
-
-ExampleComponent('Example Component');
---
+++ src/components/Header/Header.js
@@ -0,0 +1,28 @@
+
+export default class Header extends HTMLElement {
+    constructor() {
+        super();
+
+        const shadowRoot = this.attachShadow({ mode: 'open' });
+
+        shadowRoot.innerHTML = `
+            <style></style>
+
+            <header>
+                <nav>
+                    <ul>
+                        <slot></slot>
+                    </ul>
+                </nav>
+            </header>
+        `;
+    }
+
+    connectedCallback() {
+        // ...
+    }
+
+    attributeChangedCallback() {
+        // ...
+    }
+}
