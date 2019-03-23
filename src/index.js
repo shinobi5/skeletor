c72b9a63bbacb3683efea6e2b0bfd6cc2bbd4e3a
David White <david_white@bnz.co.nz>
2019-03-24 00:18:28 +1300

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import Header from '/components/Header/Header.js';
 
+// Register components
+customElements.define('skl-header', Header);
+
 // Register service worker
 // if('serviceWorker' in navigator) {
 //     navigator.serviceWorker.register('service-worker.js');
 // };
-
-// Register components
-customElements.define('skl-header', Header);
