266881a070959a8c884aaad24cb61ff8421ef741
David White <david_white@bnz.co.nz>
2019-03-23 15:58:22 +1300

    add pwa core

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import Header from '/components/Header/Header.js';
 
+// Registering service worker
+if('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('/sw.js');
+};
+
+// Register components
 customElements.define('skl-header', Header);
---
+++ sw.js
@@ -0,0 +1,45 @@
+// Files to cache
+const cacheName = 'skeletor-v1';
+const contentToCache = [
+    '/',
+    '/index.html',
+    '/index.js',
+    '/style.css',
+    '/favicon.ico',
+    '/icons/icon-32.png',
+    '/icons/icon-64.png',
+    '/icons/icon-96.png',
+    '/icons/icon-128.png',
+    '/icons/icon-168.png',
+    '/icons/icon-192.png',
+    '/icons/icon-256.png',
+    '/icons/icon-512.png'
+];
+
+// Installing service worker
+self.addEventListener('install', function(event) {
+    console.log('[Service Worker] Install');
+    event.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[Service Worker] Caching all: app shell and content');
+            return cache.addAll(contentToCache);
+        })
+    );
+});
+
+// Fetching content using service worker
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(r => {
+            console.log(`[Service Worker] Fetching resource: ${event.request.url}`);
+
+            return r || fetch(event.request).then(response => {
+                return caches.open(cacheName).then(cache => {
+                    console.log(`[Service Worker] Caching new resource: ${event.request.url}`);
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
