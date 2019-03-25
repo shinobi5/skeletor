44a7505935a53d304d73e87635a18af931c7a18f
David White <david_white@bnz.co.nz>
2019-03-25 21:59:29 +1300

    update test template

--- scripts/templates/test.js
+++ scripts/templates/test.js
@@ -1,6 +1,6 @@
 module.exports = componentName => `import ${componentName} from './${componentName}';
 
-customElements.define('x-${componentName}', ${componentName});
+console.log(${componentName});
 
 test('two plus two is four', () => {
     expect(2 + 2).toBe(4);
