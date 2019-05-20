414824043fba8b8923e53f23c1c68d0c3c0c53f5
David White <david_white@bnz.co.nz>
2019-05-20 21:21:03 +1200

    update unit test boilerplate

--- scripts/templates/test.js
+++ scripts/templates/test.js
@@ -1,9 +1,2 @@
 module.exports = component => `import ${component} from './${component}';
-
-console.log(${component});
-
-test('two plus two is four', () => {
-    expect(2 + 2).toBe(4);
-});
-
 `;
