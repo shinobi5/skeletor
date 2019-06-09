499f1bdcf32ac99082585b0ef9236fd65f809ca2
David White <7435552+shinobi5@users.noreply.github.com>
2019-06-09 21:22:12 +1200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // TODO: conditionally load service worker based 
 // on environment etc ...
 
-// Register service worker
+// register service worker
 // if('serviceWorker' in navigator) {
 //     navigator.serviceWorker.register('service-worker.js');
 // };
