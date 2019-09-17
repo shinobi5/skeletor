181e5aa2cc3c7f20baf15fdebe193bf31bcf0b04
David White <david_white@bnz.co.nz>
2019-09-17 20:32:58 +1200

    Update copy script

--- scripts/copy.js
+++ scripts/copy.js
@@ -1,25 +1,3 @@
 const fs = require('fs-extra');
 
-const src = 'src';
-const dest = 'build';
-
-const files = [
-    'index.html',
-    'app.js',
-    'service-worker.js',
-    'manifest.json',
-    'styles.css',
-    'favicon.ico',
-];
-
-const folders = ['js', 'font', 'img'];
-
-const bundle = [...files, ...folders];
-
-const copyToBuild = (files, src, dest) => {
-    files.map(file => {
-        fs.copySync(`${src}/${file}`, `${dest}/${file}`);
-    });
-};
-
-copyToBuild(bundle, src, dest);
+fs.copySync('src', 'build');
