ac182152839f4af6f8e83a5cbc1bcec0d9fb18d7
David White <david_white@bnz.co.nz>
2019-10-27 13:26:43 +1300

    Bring back lit template

---
+++ scripts/templates/component-lit.js
@@ -0,0 +1,28 @@
+module.exports = (ComponentName, elementName, elementPrefix) => {
+    return `import { html, render } from '../../modules/lit-html.js';
+
+export class ${ComponentName} extends HTMLElement {
+    constructor() {
+        super();
+        this.root = this.attachShadow({ mode: 'open' });
+    }
+
+    connectedCallback() {
+        render(this.render(), this.root);
+    }
+
+    render() {
+        return html\`
+            <style>
+                :host {
+                    display: block;
+                }
+            </style>
+            <slot></slot>
+        \`;
+    }
+}
+
+customElements.define('${elementPrefix}-${elementName.toLowerCase()}', ${ComponentName});
+`;
+};
