42ae4822557a577ebb6aa0950212a806abdf9635
David White <david_white@bnz.co.nz>
2019-11-02 13:48:39 +1300

    Update webpack options

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,7 +1,15 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+var ImageminPlugin = require('imagemin-webpack-plugin').default;
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+    .BundleAnalyzerPlugin;
 const path = require('path');
 
+const devMode = process.env.NODE_ENV !== 'production';
 const ROOT = process.cwd();
 
 module.exports = {
@@ -10,12 +18,70 @@ module.exports = {
     },
     output: {
         path: path.join(ROOT, '/build'),
-        filename: '[name].bundle.js',
+        filename: '[name].[hash].bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../',
+                            hmr: devMode,
+                        },
+                    },
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader'],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: ['file-loader'],
+            },
+        ],
+    },
+    optimization: {
+        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+        splitChunks: {
+            cacheGroups: {
+                styles: {
+                    name: 'styles',
+                    test: /\.css$/,
+                    chunks: 'all',
+                    enforce: true,
+                },
+            },
+        },
     },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: './src/index.html',
         }),
+        new MiniCssExtractPlugin({
+            filename: devMode ? '[name].css' : '[name].[hash].css',
+            chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+            ignoreOrder: false,
+        }),
+        new CopyPlugin([
+            {
+                from: path.join(ROOT, '/src'),
+                to: path.join(ROOT, '/build'),
+            },
+        ]),
+        new ImageminPlugin({
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            disable: devMode,
+            pngquant: {
+                quality: '95-100',
+            },
+        }),
+        // new BundleAnalyzerPlugin(),
     ],
 };
