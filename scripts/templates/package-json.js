f28d973b05c4b16eb78c8dbf77cffbf2205d1b97
David White <david_white@bnz.co.nz>
2019-12-18 15:58:27 +1300

    Add dependencies config

--- scripts/templates/package-json.js
+++ scripts/templates/package-json.js
@@ -1,3 +1,5 @@
+const dependencies = require('../../config/dependencies.js/index.js');
+
 module.exports = (
     bundler,
     bundlerType,
@@ -26,63 +28,141 @@ module.exports = (
     const start = `"start": "npm-run-all --parallel prettier:watch server:dev"`;
     const startCSS = `"start": "yarn css:concat && npm-run-all --parallel prettier:watch css:watch server:dev"`;
 
-    const rollup = bundler && bundlerType === 'rollup' ? true : false;
-    const webpack = bundler && bundlerType === 'webpack' ? true : false;
-    const redux = state && stateType === 'redux' ? true : false;
-    const beedle = state && stateType === 'beedle' ? true : false;
+    const rollup = bundler && bundlerType === 'rollup';
+    const webpack = bundler && bundlerType === 'webpack';
+    const redux = state && stateType === 'redux';
+    const beedle = state && stateType === 'beedle';
 
     return `{
     "name": "${projectName.toLowerCase()}",
     "description": "${description}",
     "dependencies": {
-        "@babel/runtime": "^7.6.3",
-        ${beedle ? `"beedle": "^0.8.1",` : ''}
-        ${redux ? `"redux": "^4.0.4",` : ''}
-        ${router ? `"router-component": "^0.8.0",` : ''}
-        "haunted": "^4.5.4",
-        "lit-html": "^1.1.0"
+        "@babel/runtime": "${dependencies.babelRuntime}",
+        ${beedle ? `"beedle": "${dependencies.beedle}",` : ''}
+        ${redux ? `"redux": "${dependencies.redux}",` : ''}
+        ${router ? `"router-component": "${dependencies.router}",` : ''}
+        "haunted": "${dependencies.haunted}",
+        "lit-html": "${dependencies.litHtml}"
     },
     "devDependencies": {
-        "@babel/cli": "^7.4.4",
-        "@babel/core": "^7.4.5",
-        "@babel/plugin-transform-runtime": "^7.6.2",
-        "@babel/preset-env": "^7.4.5",
-        "@pika/web": "^0.4.3",
-        "clean-css-cli": "^4.2.1",
-        "colors": "^1.4.0",
-        "fs-extra": "^8.0.1",
-        "glob": "^7.1.3",
-        "hankey": "^0.0.3",
-        "husky": "^2.4.1",
-        "imagemin-cli": "^4.0.1",
-        "live-server": "^1.2.1",
-        "npm-run-all": "^4.1.5",
-        "onchange": "^5.2.0",
-        "prettier": "1.18.2",
-        "pretty-quick": "^1.11.0",
-        ${rollup ? `"rollup": "^1.26.3",` : ''}
-        ${rollup ? `"rollup-plugin-serve": "^1.0.1",` : ''}
-        ${rollup ? `"rollup-plugin-clear": "^2.0.7",` : ''}
-        ${rollup ? `"rollup-plugin-commonjs": "^10.1.0",` : ''}
-        ${rollup ? `"rollup-plugin-copy": "^3.1.0",` : ''}
-        ${rollup ? `"rollup-plugin-generate-html-template": "^1.5.0",` : ''}
-        ${rollup ? `"rollup-plugin-node-resolve": "^5.2.0",` : ''}
-        ${rollup ? `"rollup-plugin-terser": "^5.1.2",` : ''}
-        ${webpack ? `"webpack": "^4.41.2",` : ''}
-        ${webpack ? `"webpack-cli": "^3.3.10",` : ''}
-        ${webpack ? `"webpack-merge": "^4.2.2",` : ''}
-        ${webpack ? `"clean-webpack-plugin": "^3.0.0",` : ''}
-        ${webpack ? `"webpack-bundle-analyzer": "^3.6.0",` : ''}
-        ${webpack ? `"webpack-dev-server": "^3.9.0",` : ''}
-        ${webpack ? `"optimize-css-assets-webpack-plugin": "^5.0.3",` : ''}
-        ${webpack ? `"mini-css-extract-plugin": "^0.8.0",` : ''}
-        ${webpack ? `"imagemin-webpack-plugin": "^2.4.2",` : ''}
-        ${webpack ? `"html-webpack-plugin": "^3.2.0",` : ''}
-        ${webpack ? `"file-loader": "^4.2.0",` : ''}
-        ${webpack ? `"css-loader": "^3.2.0",` : ''}
-        ${webpack ? `"copy-webpack-plugin": "^5.0.5",` : ''}
-        ${webpack ? `"babel-loader": "^8.0.6",` : ''}
-        "prompt": "^1.0.0"
+        "@babel/cli": "${dependencies.dev.babel.cli}",
+        "@babel/core": "${dependencies.dev.babel.core}",
+        "@babel/plugin-transform-runtime": "${
+            dependencies.dev.babel.pluginTransformRuntime
+        }",
+        "@babel/preset-env": "${dependencies.dev.babel.presetEnv}",
+        "@pika/web": "${dependencies.dev.pika.web}",
+        "clean-css-cli": "${dependencies.dev.cleanCSSCli}",
+        "colors": "${dependencies.dev.colors}",
+        "fs-extra": "${dependencies.dev.fsExtra}",
+        "glob": "${dependencies.dev.glob}",
+        "hankey": "${dependencies.dev.hankey}",
+        "husky": "${dependencies.dev.husky}",
+        "imagemin-cli": "${dependencies.dev.imageMinCli}",
+        "live-server": "${dependencies.dev.liveServer}",
+        "npm-run-all": "${dependencies.dev.npmRunAll}",
+        "onchange": "${dependencies.dev.onChange}",
+        "prettier": "${dependencies.dev.prettier}",
+        "pretty-quick": "${dependencies.dev.prettyQuick}",
+        ${rollup ? `"rollup": "${dependencies.dev.rollup.rollup}",` : ''}
+        ${
+            rollup
+                ? `"rollup-plugin-serve": "${dependencies.dev.rollup.pluginServer}",`
+                : ''
+        }
+        ${
+            rollup
+                ? `"rollup-plugin-clear": "${dependencies.dev.rollup.pluginClear}",`
+                : ''
+        }
+        ${
+            rollup
+                ? `"rollup-plugin-commonjs": "${dependencies.dev.rollup.pluginCommonjs}",`
+                : ''
+        }
+        ${
+            rollup
+                ? `"rollup-plugin-copy": "${dependencies.dev.rollup.pluginCopy}",`
+                : ''
+        }
+        ${
+            rollup
+                ? `"rollup-plugin-generate-html-template": "${dependencies.dev.rollup.pluginGenerateHtmlTemplate}",`
+                : ''
+        }
+        ${
+            rollup
+                ? `"rollup-plugin-node-resolve": "${dependencies.dev.rollup.pluginNodeResolve}",`
+                : ''
+        }
+        ${
+            rollup
+                ? `"rollup-plugin-terser": "${dependencies.dev.rollup.pluginTerser}",`
+                : ''
+        }
+        ${webpack ? `"webpack": "${dependencies.dev.webpack.webpack}",` : ''}
+        ${webpack ? `"webpack-cli": "${dependencies.dev.webpack.cli}",` : ''}
+        ${
+            webpack
+                ? `"webpack-merge": "${dependencies.dev.webpack.merge}",`
+                : ''
+        }
+        ${
+            webpack
+                ? `"clean-webpack-plugin": "${dependencies.dev.webpack.cleanWbpackPlugin}",`
+                : ''
+        }
+        ${
+            webpack
+                ? `"webpack-bundle-analyzer": "${dependencies.dev.webpack.bundleAnalyzer}",`
+                : ''
+        }
+        ${
+            webpack
+                ? `"webpack-dev-server": "${dependencies.dev.webpack.devServer}",`
+                : ''
+        }
+        ${
+            webpack
+                ? `"optimize-css-assets-webpack-plugin": "${dependencies.dev.webpack.optimizeCssAssetsWebpackPlugin}",`
+                : ''
+        }
+        ${
+            webpack
+                ? `"mini-css-extract-plugin": "${dependencies.dev.webpack.miniCssExtractPlugin}",`
+                : ''
+        }
+        ${
+            webpack
+                ? `"imagemin-webpack-plugin": "${dependencies.dev.webpack.imageminWebpackPlugin}",`
+                : ''
+        }
+        ${
+            webpack
+                ? `"html-webpack-plugin": "${dependencies.dev.webpack.htmlWebpackPlugin}",`
+                : ''
+        }
+        ${
+            webpack
+                ? `"file-loader": "${dependencies.dev.webpack.fileLoader}",`
+                : ''
+        }
+        ${
+            webpack
+                ? `"css-loader": "${dependencies.dev.webpack.cssLoader}",`
+                : ''
+        }
+        ${
+            webpack
+                ? `"copy-webpack-plugin": "${dependencies.dev.webpack.copyWebpackPlugin}",`
+                : ''
+        }
+        ${
+            webpack
+                ? `"babel-loader": "${dependencies.dev.webpack.babelLoader}",`
+                : ''
+        }
+        "prompt": "${dependencies.dev.prompt}"
     },
     "scripts": {
         "babel": "npx babel src -d build --copy-files",
