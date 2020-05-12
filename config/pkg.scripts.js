4f9a836e3fd89b9f134f94e7fc650c131540da1f
shinobi5 <shinobi5@shinobi5stealth.hub>
2020-05-13 01:17:29 +1200

    convert yarn commands to npm

--- config/pkg.scripts.js
+++ config/pkg.scripts.js
@@ -1,16 +1,16 @@
 module.exports = {
     build: {
-        basic: `"build": "yarn imagemin && yarn babel",`,
+        basic: `"build": "npm run imagemin && npm run babel",`,
         rollup: `"build": "rollup --config config/rollup.prod.js",`,
         webpack: `"build": "webpack --config config/webpack.prod.js",`,
-        css: `"build": "yarn css:concat && yarn css:minify && yarn imagemin && yarn babel",`,
-        rollupCss: `"build": "yarn css:concat && yarn css:minify && rollup --config config/rollup.prod.js",`,
-        webpackCss: `"build": "yarn css:concat && yarn css:minify && webpack --config config/webpack.prod.js",`,
+        css: `"build": "npm run css:concat && npm run css:minify && npm run imagemin && npm run babel",`,
+        rollupCss: `"build": "npm run css:concat && npm run css:minify && rollup --config config/rollup.prod.js",`,
+        webpackCss: `"build": "npm run css:concat && npm run css:minify && webpack --config config/webpack.prod.js",`,
     },
     css: {
         concat: `"css:concat": "cat src/css/settings/* src/css/global/* src/css/elements/* src/css/components/* src/css/utilities/* > src/styles.css",`,
         minify: `"css:minify": "cleancss -o src/styles.css src/styles.css",`,
-        watch: `"css:watch": "onchange 'src/css/**/*' -- yarn css:concat",`,
+        watch: `"css:watch": "onchange 'src/css/**/*' -- npm run css:concat",`,
     },
     server: {
         dev: `"server:dev": "live-server --open=src",`,
@@ -19,6 +19,6 @@ module.exports = {
     },
     start: {
         basic: `"start": "npm-run-all --parallel prettier:watch server:dev"`,
-        css: `"start": "yarn css:concat && npm-run-all --parallel prettier:watch css:watch server:dev"`,
+        css: `"start": "npm run css:concat && npm-run-all --parallel prettier:watch css:watch server:dev"`,
     },
 };
