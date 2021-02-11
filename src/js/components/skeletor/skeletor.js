1741bee47eb51e3179233c2ac96d9c16e36a87c7
shinobi5 <shinobi5@pm.me>
2021-02-11 21:35:08 +1300

    refactor: remove global css for skeletor element

--- src/js/components/skeletor/skeletor.js
+++ src/js/components/skeletor/skeletor.js
@@ -1,20 +1,41 @@
 import { component, html } from "https://cdn.skypack.dev/haunted";
-import css from 'https://cdn.skypack.dev/csz';
-import "../title/title.js";
 
 export const skeletor = () => {
   return html`
+    <style>
+      :host {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI',
+          'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans',
+          'Droid Sans', 'Helvetica Neue', sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+        background-color: #110e0c;
+      }
+      img {
+        width: 180px;
+      }
+      h1 {
+        color: white;
+        text-align: center;
+        font-weight: 900;
+        letter-spacing: 0.5px;
+        margin: 0;
+      }
+    </style>
+
     <div>
       <img
-        class=${css`
-          width: 180px;
-        `}
         src="img/skeletor.png"
         alt="Image of Skeletor, the lead villain, from Masters of the Universe"
       />
-      <x-title>Skeletor</x-title>
+      <h1>Skeletor</h1>
     </div>
   `;
 };
 
-customElements.define("x-skeletor", component(skeletor, { useShadowDOM: false }));
+customElements.define("x-skeletor", component(skeletor));
